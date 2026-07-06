ee4c77c13f63262723ed3b8ef28e60756a815182
TanujWebDev <tanujjangra780@gmail.com>
2026-07-06 18:56:05 +0530

    Add root route handler

--- backend/server.js
+++ backend/server.js
@@ -22,4 +22,8 @@ app.use('/api/admin', adminRouter)
 app.use('/api/doctor', doctorRouter)
 app.use('/api/user', userRouter)
 
+app.get('/', (req, res) => {
+  res.send('CuraDoc API Working')
+})
+
 app.listen(port, () => console.log('Server started on port', port))
